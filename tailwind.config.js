6627c29b4d13e375d85fb628943b744e51a19fa6
Mr.Pratchayanan <Yanperm@users.noreply.github.com>
2022-01-03 13:51:54 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,4 +6,25 @@ module.exports = {
   plugins: [
     require('daisyui'),
   ],
+  daisyui: {
+    themes: [
+      {
+        'mytheme': { // custom theme
+          'primary' : '#ea5234',
+          'primary-focus' : '#d43616',
+          'primary-content' : '#ffffff',
+          // other colors
+        },
+        'myothertheme': { // custom theme
+          'primary' : '#007ebd',
+          'primary-focus' : '#005c8a',
+          'primary-content' : '#ffffff',
+          // other colors
+        },
+      },
+      'dark', // and some pre-defined theme
+      'forest',
+      'synthwave'
+    ],
+  },
 }
